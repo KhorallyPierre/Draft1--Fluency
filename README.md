d2619e7d23791728896b8622a9f454576d48d600
Khorally Pierre <kvpierre@gmail.com>
2021-03-27 22:00:45 -0400

    demo day files

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Fluency-Demo-Day
+
+Welcome to my Demo Day project! In honor of my love of languages and learning, I've created an application where members can video chat with individuals fluent in the language that they would like to learn. Members get rated based on their level of helpfulness. The more helpful a user it, the higher the score displayed on their profile. These individuals get preferred matches, which encourage users to be as helpful as possible. The idea is to help others learn a new language while also using them as a resource to do the same. Users spend half the session in one language, and the other half in the other language.
+
+![Fluency Home Page](background2.jpg)
+
+# Tech used:
+HTML, CSS, JavaScript, and MongoDB. More specifically, I used MongoDB to store the documents storing the randomization of user interactions (I incorporated Math.floor(Math.random())) into my CRUD.
+
+
+
+# Lessons Learned:
+Nothing in tech is daunting as long as you make it fun! I love learning new languages (fun fact: I speak three languages, one of which I learned studying abroad) so using this theme has really made me feel like I'm actually making a difference in the lives of those around me that are passionate about language learning like I am.
+
+# My Other Projects
+Take a look at these couple examples that I have in my own portfolio:
+
+https://wonderful-easley-4bdc4b.netlify.app/
+
+https://hopeful-hamilton-96af68.netlify.app/
+
+https://zealous-dubinsky-e78c32.netlify.app
---
+++ app/routes.js
@@ -0,0 +1,199 @@
+module.exports = function(app, passport, db) {
+
+  // normal routes ===============================================================
+
+  // show the home page (will also have our login links)
+
+
+  // PROFILE SECTION =========================
+  //     Create (POST) - Make match between users
+  // url to access this information localhost:1000/pair/Spanish
+  app.post('/userProfile', isLoggedIn, function(req, res) {
+    const profileObject = {
+      email: req.body.email,
+      userName: req.body.userName,
+      proficiency: req.body.proficiency,
+      language: req.body.language,
+      learning: req.body.learning
+    }
+    db.collection('userProfile').insertOne(profileObject, (err, result) => {
+      if (err) {
+        res.redirect('request failed, try again')
+      } else {
+        res.redirect('profile.html')
+      }
+    })
+  });
+
+  //
+  // who speaks desired langugage gets chosen /read
+
+  app.get('/pair/:language', /*isLoggedIn,*/ function(req, res) {
+    console.log('language', req.params.language)
+    db.collection('userProfile').find({language: req.params.language}).toArray((err, result) => {
+      console.log('paired choices', result)
+      const randomPair = Math.floor(Math.random() * result.length)
+      if (err) return console.log(err)
+      res.send(result[randomPair])
+    })
+  });
+
+  app.get('/profile', isLoggedIn, function(req, res) {
+    db.collection('messages').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      res.render('profile.html', {
+        user: req.user,
+
+      })
+    })
+  });
+
+  // CREATE A USER IN PROCESS OF PAIRING  ==================================
+
+
+
+  // LOGOUT ==============================
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // message board routes ===============================================================
+
+  app.post('/messages', (req, res) => {
+    db.collection('messages').save({
+      name: req.body.name,
+      msg: req.body.msg,
+      thumbUp: 0,
+      thumbDown: 0
+    }, (err, result) => {
+      if (err) return console.log(err)
+      console.log('saved to database')
+      res.redirect('/profile')
+    })
+  })
+
+  app.put('/messages', (req, res) => {
+    db.collection('messages')
+      .findOneAndUpdate({
+        name: req.body.name,
+        msg: req.body.msg
+      }, {
+        $set: {
+          thumbUp: req.body.thumbUp + 1
+        }
+      }, {
+        sort: {
+          _id: -1
+        },
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+  })
+
+  app.put('/messages/down', (req, res) => {
+    db.collection('messages')
+      .findOneAndUpdate({
+        name: req.body.name,
+        msg: req.body.msg
+      }, {
+        $set: {
+          thumbUp: req.body.thumbUp - 1
+        }
+      }, {
+        sort: {
+          _id: -1
+        },
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+  })
+
+  app.delete('/messages', (req, res) => {
+    db.collection('messages').findOneAndDelete({
+      name: req.body.name,
+      msg: req.body.msg
+    }, (err, result) => {
+      if (err) return res.send(500, err)
+      res.send('Message deleted!')
+    })
+  })
+
+  // =============================================================================
+  // AUTHENTICATE (FIRST LOGIN) ==================================================
+  // =============================================================================
+
+  // locally --------------------------------
+  // LOGIN ===============================
+  // show the login form
+  app.get('/login', function(req, res) {
+    res.render('login.ejs', {
+      message: req.flash('loginMessage')
+    });
+  });
+
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/login', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
+
+  // SIGNUP =================================
+  // show the signup form
+  app.get('/signup', function(req, res) {
+    res.render('signup.ejs', {
+      message: req.flash('signupMessage')
+    });
+  });
+
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    // COMMENTED OUT to allow the function below to run after passport creates a user
+    // successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/login', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }), function  signupCallback(req, res, err) {
+    console.log("signup callback, user:", req.user, err);
+    db.collection('userProfile').save({ email: req.body.email, userId: req.user._id, color: req.body.color}, (err, result) => {
+      if (err) return console.log(err)
+      console.log('saved to database userProfile')
+      res.redirect('/profile')
+    })
+  });
+  // app.post('/signup', passport.authenticate('local-signup', {
+  //   successRedirect: '/profile', // redirect to the secure profile section
+  //   failureRedirect: '/signup', // redirect back to the signup page if there is an error
+  //   failureFlash: true // allow flash messages
+  // }));
+
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
+
+  // local -----------------------------------
+  app.get('/unlink/local', isLoggedIn, function(req, res) {
+    var user = req.user;
+    user.local.email = undefined;
+    user.local.password = undefined;
+    user.save(function(err) {
+      res.redirect('/profile');
+    });
+  });
+
+};
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+
+  res.redirect('/');
+}
