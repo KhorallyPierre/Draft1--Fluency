57a98605d6d5effe89219eeb8f324ea900cb42d5
Khorally Pierre <kvpierre@gmail.com>
2021-04-13 11:27:25 -0400

    updates

--- app/routes.js
+++ app/routes.js
@@ -6,8 +6,9 @@ module.exports = function(app, passport, db) {
 
 
   // PROFILE SECTION =========================
-  //     Create (POST) - Make match between users
+  //  Create (POST) - Make match between users
   // url to access this information localhost:1000/pair/Spanish
+  // create
   app.post('/userProfile', isLoggedIn, function(req, res) {
     const profileObject = {
       email: req.body.email,
@@ -25,30 +26,166 @@ module.exports = function(app, passport, db) {
     })
   });
 
-  //
-  // who speaks desired langugage gets chosen /read
 
+// UPLOAD PROFILE PICTURE // ======================
+
+app.post('/picture', (req, res) => {
+  if(req.files){
+    console.log(req.files)
+    var file = req.files.file
+    var fileName = decodeURIComponent(file.name)
+    console.log(fileName)
+
+    file.mv('public/uploads/'+fileName, function (err){
+      if (err) {
+        res.send(err)
+      } else {
+
+        res.redirect('/userProfile')
+      }
+    })
+    db.collection('picture').save({name: req.body.name, img: "/uploads/" + fileName, thumbUp: 0, thumbDown:0}, (err, result) => {
+      if (err) return console.log(err)
+      console.log('saved to database')
+
+    })
+  }
+
+})
+
+
+
+app.delete('/picture', (req, res) => {
+  console.log("trying to delete image", decodeURIComponent(req.body.img))
+
+  db.collection('picture').findOneAndDelete({img: decodeURIComponent(req.body.img)} , (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('picture deleted!')
+  })
+})
+//==============//===========================================
+
+
+// ======================= PAIRING OF USERS THAT WANT RESOURCES THE OTHER ONE HAS
+
+  // who speaks desired langugage gets chosen /read
+  // get is reading
   app.get('/pair/:language', /*isLoggedIn,*/ function(req, res) {
     console.log('language', req.params.language)
-    db.collection('userProfile').find({language: req.params.language}).toArray((err, result) => {
-      console.log('paired choices', result)
+    db.collection('userProfile').find({
+      languages: {
+        $elemMatch: {
+          language: req.params.language,
+          teachOrLearn: "teach"
+        }
+      }
+    }).toArray((err, result) => {
+      // console.log('paired choices', result)
       const randomPair = Math.floor(Math.random() * result.length)
       if (err) return console.log(err)
       res.send(result[randomPair])
     })
   });
 
-  app.get('/profile', isLoggedIn, function(req, res) {
-    db.collection('messages').find().toArray((err, result) => {
-      if (err) return console.log(err)
-      res.render('profile.html', {
-        user: req.user,
 
+  // THIS PUTS LANGUAGE ON USER PROFILE
+  app.post('/addLanguage', /*isLoggedIn,*/ function(req, res) {
+    console.log('addLanguage', req.body)
+    const fluency = parseInt(req.body.country) + parseInt(req.body.teach) + parseInt(req.body.help)
+    const languageObject = {
+      language: req.body.language,
+      teachOrLearn: req.body.teachOrLearn,
+      fluency: fluency
+
+    }
+    db.collection('userProfile').findOneAndUpdate(
+      //filter
+      {
+        email: req.user.local.email
+      },
+      //update
+      {
+        $push: {
+          "languages": languageObject
+        }
+      }, (err, result) => {
+        if (err) {
+          console.log("addLanguage", err)
+          res.redirect('/userProfile')
+        } else {
+          res.redirect('/profile')
+        }
       })
-    })
   });
 
-  // CREATE A USER IN PROCESS OF PAIRING  ==================================
+  // displaying profile where video message is coming from ???
+  app.get('/profile', isLoggedIn, function(req, res) {
+    db.collection('userProfile').findOne({
+      email: req.user.local.email
+    }, (err, result) => {
+      if (err) return console.log(err)
+      db.collection('requests').find({
+        status: "waiting"
+      }).toArray((err, requests) => {
+        console.log('chat requests', requests)
+        res.render('profile.ejs', {
+          user: req.user,
+          userProfile: result,
+          requests: requests
+
+        })
+      })
+    });
+  })
+  app.get('/', function(req, res) {
+    res.render('index.ejs', {
+
+    })
+
+  });
+
+  // ============ DIRECT TO VIDEO ROOM WHERE STUDENT WAITS FOR TEACHER
+
+  //
+  // implement tips on how to be a good teacher or how to learn
+  // countdown
+  // alert: "we've found you a match!"
+
+  app.get('/profile', isLoggedIn, function(req, res) {
+    db.collection('messages').findOne({
+      email: req.user.local.email
+    }, (err, result) => {
+      if (err) return console.log(err)
+      // inside find() we need a filter to find messages addressed to a
+      //specific user (whoever is logged in)
+      db.collection('messages').find({
+        message: req.user.local.message
+      }).toArray((err, messages) => {
+        // not sure about my console.log below
+        console.log('the displayed message is', req.user.local.message, messages)
+        res.render('profile.ejs', {
+          user: req.user,
+          userProfile: result,
+          // not sure about below
+          messages: messages
+
+        })
+      })
+    });
+  })
+
+
+  // other person (TEACHER) gets an alert that someone would like to chat
+  // - they can say yes or no and they get brought to the same room
+  //push button, then you're connected
+
+  // put TEACHER INTO CHAT ROOM WITH WAITING STUDENT
+
+
+
+
+  // figure out web rtc implementation
+
 
 
 
@@ -58,70 +195,107 @@ module.exports = function(app, passport, db) {
     res.redirect('/');
   });
 
-  // message board routes ===============================================================
+  // // message board routes ===============================================================
+  //
+  // app.post('/messages', (req, res) => {
+  //   db.collection('messages').save({
+  //     name: req.body.name,
+  //     msg: req.body.msg,
+  //     thumbUp: 0,
+  //     thumbDown: 0
+  //   }, (err, result) => {
+  //     if (err) return console.log(err)
+  //     console.log('saved to database')
+  //     res.redirect('/profile')
+  //   })
+  // })
+  //
+  // app.put('/messages', (req, res) => {
+  //   db.collection('messages')
+  //     .findOneAndUpdate({
+  //       name: req.body.name,
+  //       msg: req.body.msg
+  //     }, {
+  //       $set: {
+  //         thumbUp: req.body.thumbUp + 1
+  //       }
+  //     }, {
+  //       sort: {
+  //         _id: -1
+  //       },
+  //       upsert: true
+  //     }, (err, result) => {
+  //       if (err) return res.send(err)
+  //       res.send(result)
+  //     })
+  // })
+  //
+  // app.put('/messages/down', (req, res) => {
+  //   db.collection('messages')
+  //     .findOneAndUpdate({
+  //       name: req.body.name,
+  //       msg: req.body.msg
+  //     }, {
+  //       $set: {
+  //         thumbUp: req.body.thumbUp - 1
+  //       }
+  //     }, {
+  //       sort: {
+  //         _id: -1
+  //       },
+  //       upsert: true
+  //     }, (err, result) => {
+  //       if (err) return res.send(err)
+  //       res.send(result)
+  //     })
+  // })
 
-  app.post('/messages', (req, res) => {
-    db.collection('messages').save({
-      name: req.body.name,
-      msg: req.body.msg,
-      thumbUp: 0,
-      thumbDown: 0
-    }, (err, result) => {
-      if (err) return console.log(err)
-      console.log('saved to database')
-      res.redirect('/profile')
-    })
-  })
+  // app.delete('/messages', (req, res) => {
+  //   db.collection('messages').findOneAndDelete({
+  //     name: req.body.name,
+  //     msg: req.body.msg
+  //   }, (err, result) => {
+  //     if (err) return res.send(500, err)
+  //     res.send('Message deleted!')
+  //   })
+  // })
 
-  app.put('/messages', (req, res) => {
-    db.collection('messages')
-      .findOneAndUpdate({
-        name: req.body.name,
-        msg: req.body.msg
-      }, {
-        $set: {
-          thumbUp: req.body.thumbUp + 1
+  // =================================== PAIRING PPL TO VIDEO CHAT
+
+
+
+  //needs to create video request in request collection on mongodb
+  // needs to redirect user to a res.redirect to the video page
+  // implement github code
+
+  app.post('/pair', isLoggedIn, function(req, res) {
+    const profileObject = {
+      language: req.body.language,
+      learning: req.body.learning
+    }
+    db.collection('requests').insertOne(profileObject, (err, result) => {
+      db.collection('userProfile').findOne({
+        languages: {0:{language:'English'}}
+        // {  $elemMatch: {
+            // language: req.params.language,
+            // teachOrLearn: "teach"
+        //   }
+        // }
+        // email: req.user.email
+      }, (error, pairing) => {
+        console.log('HELOOOOOOO', pairing)
+        if (error) {
+          res.redirect('request failed, try again')
+        } else {
+          res.redirect('/profile')
         }
-      }, {
-        sort: {
-          _id: -1
-        },
-        upsert: true
-      }, (err, result) => {
-        if (err) return res.send(err)
-        res.send(result)
+        // if () ? //insert button here//  : "could not find a match"
       })
-  })
 
-  app.put('/messages/down', (req, res) => {
-    db.collection('messages')
-      .findOneAndUpdate({
-        name: req.body.name,
-        msg: req.body.msg
-      }, {
-        $set: {
-          thumbUp: req.body.thumbUp - 1
-        }
-      }, {
-        sort: {
-          _id: -1
-        },
-        upsert: true
-      }, (err, result) => {
-        if (err) return res.send(err)
-        res.send(result)
-      })
-  })
-
-  app.delete('/messages', (req, res) => {
-    db.collection('messages').findOneAndDelete({
-      name: req.body.name,
-      msg: req.body.msg
-    }, (err, result) => {
-      if (err) return res.send(500, err)
-      res.send('Message deleted!')
     })
-  })
+  });
+
+
 
   // =============================================================================
   // AUTHENTICATE (FIRST LOGIN) ==================================================
@@ -157,9 +331,13 @@ module.exports = function(app, passport, db) {
     // successRedirect: '/profile', // redirect to the secure profile section
     failureRedirect: '/login', // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
-  }), function  signupCallback(req, res, err) {
+  }), function signupCallback(req, res, err) {
     console.log("signup callback, user:", req.user, err);
-    db.collection('userProfile').save({ email: req.body.email, userId: req.user._id, color: req.body.color}, (err, result) => {
+    db.collection('userProfile').save({
+      email: req.body.email,
+      userId: req.user._id,
+      languages: []
+    }, (err, result) => {
       if (err) return console.log(err)
       console.log('saved to database userProfile')
       res.redirect('/profile')
